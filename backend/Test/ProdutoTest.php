0884187eba518448b4858de4a05994de5e6ec215
faustinopsy <rodrigohipnose@gmail.com>
2023-09-26 20:45:10 -0300

    modified:   backend/Test/ProdutoTest.php 	modified:   backend/Test/UserTest.php 	modified:   backend/sqlite/test_drive.db

--- backend/Test/ProdutoTest.php
+++ backend/Test/ProdutoTest.php
@@ -13,8 +13,16 @@ class ProdutoTest {
     }
 
     public function runTests() {
-        echo "CRUD tests...\n";
-        // SELECT
+        echo "CRUD tests...<br>";
+        $this->select();
+        $this->insert();
+        $this->select();
+        $this->update();
+        $this->select();
+        $this->delete();
+        $this->select();
+    }
+    public function select(){
         echo "<b>SELEÇÃO:</b>";
         $produto = $this->db->select('produtos');
         echo "Test do Select " . count($produto)  . " produtos.\n";
@@ -24,54 +32,27 @@ class ProdutoTest {
             }
         }
         echo "<hr>";
-        // INSERT
+    }
+    public function insert(){
         echo "<b>INSERÇÃO:</b>";
         $data = ['nome' => 'Café', 'preco' => '1,50', 'quantidade' => '5'];
         $result = $this->db->insert('produtos', $data);
         echo "Teste do Insert: " . ($result ? "Sucesso" : "Falha") . "\n";
         echo "<hr>";
-        // SELECT
-        echo "<b>SELEÇÃO:</b>";
-        $produto = $this->db->select('produtos');
-        echo "Test do Select " . count($produto)  . " produtos.\n";
-        foreach($produto as $produtos){
-            foreach($produtos as $key => $value){
-                echo '<br>'.$key.': '.$value;
-            }
-        }
-        echo "<hr>";
-        // UPDATE
+    }
+    public function update(){
         echo "<b>ATUALIZAÇÃO:</b>";
         $newData = ['nome' => 'Café', 'preco' => '3,50', 'quantidade' => '2'];
         $conditions = ['nome' => 'Café'];
         $result = $this->db->update('produtos', $newData, $conditions);
         echo "Update : " . ($result ? "Sucesso" : "Falha") . "\n";
         echo "<hr>";
-        // SELECT
-        echo "<b>SELEÇÃO:</b>";
-        $produto = $this->db->select('produtos');
-        echo "Test do Select " . count($produto)  . " produtos.\n";
-        foreach($produto as $produtos){
-            foreach($produtos as $key => $value){
-                echo '<br>'.$key.': '.$value;
-            }
-        }
-        echo "<hr>";
-        // DELETE
+    }
+    public function delete(){
         echo "<b>EXCLUSÃO:</b>";
         $conditions = ['nome' => 'Café'];
         $result = $this->db->delete('produtos', $conditions);
         echo "Delete : " . ($result ? "Sucesso" : "Falha") . "\n";
-        echo "<hr>";
-        // SELECT
-        echo "<b>SELEÇÃO:</b>";
-        $produto = $this->db->select('produtos');
-        echo "Test do Select " . count($produto)  . " produtos.\n";
-        foreach($produto as $produtos){
-            foreach($produtos as $key => $value){
-                echo '<br>'.$key.': '.$value;
-            }
-        }
         echo "<hr>";
     }
 }
--- backend/Test/UserTest.php
+++ backend/Test/UserTest.php
@@ -13,8 +13,16 @@ class UserTest {
     }
 
     public function runTests() {
-        echo "CRUD tests...\n";
-        // SELECT
+        echo "CRUD tests...<br>";
+        $this->select();
+        $this->insert();
+        $this->select();
+        $this->update();
+        $this->select();
+        $this->delete();
+        $this->select();
+    }
+    public function select(){
         echo "<b>SELEÇÃO:</b>";
         $user = $this->db->select('users');
         echo "Test do Select " . count($user)  . " users.\n";
@@ -24,54 +32,27 @@ class UserTest {
             }
         }
         echo "<hr>";
-        // INSERT
+    }
+    public function insert(){
         echo "<b>INSERÇÃO:</b>";
         $data = ['nome' => 'testUser', 'email' => 'testPass@xxx.com', 'senha' => '123456'];
         $result = $this->db->insert('users', $data);
         echo "Teste do Insert: " . ($result ? "Sucesso" : "Falha") . "\n";
         echo "<hr>";
-        // SELECT
-        echo "<b>SELEÇÃO:</b>";
-        $user = $this->db->select('users');
-        echo "Test do Select " . count($user)  . " users.\n";
-        foreach($user as $users){
-            foreach($users as $key => $value){
-                echo '<br>'.$key.': '.$value;
-            }
-        }
-        echo "<hr>";
-        // UPDATE
+    }
+    public function update(){
         echo "<b>ATUALIZAÇÃO:</b>";
         $newData = ['nome' => 'updatedUser'];
         $conditions = ['nome' => 'testUser'];
         $result = $this->db->update('users', $newData, $conditions);
         echo "Update : " . ($result ? "Sucesso" : "Falha") . "\n";
         echo "<hr>";
-        // SELECT
-        echo "<b>SELEÇÃO:</b>";
-        $user = $this->db->select('users');
-        echo "Test do Select " . count($user)  . " users.\n";
-        foreach($user as $users){
-            foreach($users as $key => $value){
-                echo '<br>'.$key.': '.$value;
-            }
-        }
-        echo "<hr>";
-        // DELETE
+    }
+    public function delete(){
         echo "<b>EXCLUSÃO:</b>";
         $conditions = ['nome' => 'updatedUser'];
         $result = $this->db->delete('users', $conditions);
         echo "Delete : " . ($result ? "Sucesso" : "Falha") . "\n";
-        echo "<hr>";
-        // SELECT
-        echo "<b>SELEÇÃO:</b>";
-        $user = $this->db->select('users');
-        echo "Test do Select " . count($user)  . " users.\n";
-        foreach($user as $users){
-            foreach($users as $key => $value){
-                echo '<br>'.$key.': '.$value;
-            }
-        }
         echo "<hr>";
     }
 }
