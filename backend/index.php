8b6f097452212aa58771ed1373c0dbc98005ec7c
faustinopsy <rodrigohipnose@gmail.com>
2023-10-06 10:38:28 -0300

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -4,7 +4,10 @@ require "../vendor/autoload.php";
 
 use App\Controller\UserController;
 use App\Controller\ProdutoController;
-
+use App\Model\Pessoa;
 $users = new UserController();
 $produtos = new ProdutoController();
+
+$pessoa = new Pessoa();
+echo $pessoa->create();
 
